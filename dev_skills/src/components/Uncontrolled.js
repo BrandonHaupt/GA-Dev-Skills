1bb227f6db0db279d8aae92292652595578df786
Brandon Haupt <haupt.brandon@gmail.com>
2022-12-20 19:58:49 -0600

    Update 1.5
    - added something another

--- dev_skills/src/components/Uncontrolled.js
+++ dev_skills/src/components/Uncontrolled.js
@@ -1,9 +1,20 @@
+import { useRef } from "react"
 
 
+// Uncontrolled because it's not controlled by the form
 function Uncontrolled(props){
+
+    const inputRef = useRef(null)
+
+    const handleClick = (event) => {
+        // console.log(inputRef.current) //* targets what the function is attached to
+       // console.log(inputRef.current.value) //* Grabs the value of what is being typed into the box'
+   
+    }
+
     return <div>
-        <input type="text"></input>
-        <button>Submit</button>
+        <input type="text" ref={inputRef}></input>
+        <button onClick={handleClick}>Submit</button>
     </div>
 }
 
